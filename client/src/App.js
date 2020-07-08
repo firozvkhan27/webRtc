602ac7c653d774c4b2ae8036136cd47516bbc307
firozwkhan92 <firozwkhan92@gmail.com>
2020-07-08 14:29:58 +0530

    test

---
+++ client/src/App.js
@@ -0,0 +1,215 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './App.css';
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+`;
+const SideBar = styled.div`
+flex: 0 0 80%;
+display: flex;
+justify-content: flex-start;
+`;
+
+const right =styled.div`
+  position: absolute;
+  right: 0px;
+  width: 40%;
+  border: 3px solid #73AD21;
+  padding: 10px;
+`;
+
+
+
+const Video = styled.video`
+  border: 1px solid blue;
+  width: 50%;
+  height: 50%;
+  
+`;
+const ParentDiv = styled.div`
+  & {
+    width: 100%;
+  }
+`;
+
+const ChildDiv = styled.div`
+  & {
+    display: inline-block;
+    vertical-align: text-top;
+    horizontal-align: text-right;
+    margin: 0 auto;
+  }
+`;
+function App() {
+  const [yourID, setYourID] = useState("");
+  const [users, setUsers] = useState({});
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io.connect("/");
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+      setStream(stream);
+      if (userVideo.current) {
+        userVideo.current.srcObject = stream;
+      }
+    })
+
+    socket.current.on("yourID", (id) => {
+      setYourID(id);
+    })
+    socket.current.on("allUsers", (users) => {
+      setUsers(users);
+    })
+
+    socket.current.on("hey", (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setCallerSignal(data.signal);
+    })
+  }, []);
+
+  function callPeer(id) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      config: {
+
+        iceServers: [
+            {
+                urls: "stun:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            },
+            {
+                urls: "turn:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            }
+        ]
+    },
+      stream: stream,
+    });
+
+    peer.on("signal", data => {
+      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID })
+    })
+
+    peer.on("stream", stream => {
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = stream;
+      }
+    });
+
+    socket.current.on("callAccepted", signal => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    })
+
+  }
+
+  function acceptCall() {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on("signal", data => {
+      socket.current.emit("acceptCall", { signal: data, to: caller })
+    })
+
+    peer.on("stream", stream => {
+      partnerVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+  }
+
+  let UserVideo;
+  if (stream) {
+    UserVideo = (
+      <Video playsInline muted ref={userVideo} autoPlay />
+    );
+  }
+
+  let PartnerVideo;
+  if (callAccepted) {
+    PartnerVideo = (
+      <Video playsInline ref={partnerVideo} autoPlay />
+    );
+  }
+
+  let incomingCall;
+  if (receivingCall) {
+    incomingCall = (
+      <div>
+        <h1>{caller} is calling you</h1>
+        <button onClick={acceptCall}>Accept</button>
+      </div>
+    )
+  }
+  return (
+  <div>
+
+<div class="w3-sidebar w3-bar-block w3-card" style={{width:"25%", right:"0"}}>
+  <h3 class="w3-bar-item">Participants</h3>   
+  <label>caller 1</label>      
+  < div  class="w3-bar-item w3-button">{UserVideo}</div>
+
+    <label>caller 2</label>
+  < div  class="w3-bar-item w3-button"> {PartnerVideo}</div>
+  
+</div>
+
+
+<div style={{"margin-right":"25%"}}>
+
+<div class="w3-container w3-teal">
+  <h1>Contact Info</h1>
+
+    <Container>
+      
+      <Row>
+        {Object.keys(users).map(key => {
+          if (key === yourID) {
+            return null;
+          }
+          return (
+            <button onClick={() => callPeer(key)}>Call {key}</button>
+          );
+        })}
+      </Row>
+      <Row>
+        {incomingCall}
+      </Row>
+
+  
+    
+    </Container>
+    </div>
+
+</div>
+    </div>
+  );
+}
+
+export default App;
